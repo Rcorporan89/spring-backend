2b9f5d661daedeab0807478a16130f1e033ee6da
Rickey Corporan <Rcorporan89@gmail.com>
2022-12-06 17:46:11 -0500

    working backend server

--- src/main/java/com/example/springbackend/repository/UserRepository.java
+++ src/main/java/com/example/springbackend/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
     public User getUser() {
-        User user = new User("Jose", "lvl 7 backend st", 27);
+        User user = new User("Rickey", "123 Sesame Street", 33);
         return user;
     }
 }
