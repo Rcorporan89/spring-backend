913c20847c40ba2c877d66207448b7dbd3b49238
Rickey Corporan <Rcorporan89@gmail.com>
2022-12-15 17:23:09 -0500

    adding a post resource and unique id

--- src/main/java/com/example/springbackend/repository/UserRepository.java
+++ src/main/java/com/example/springbackend/repository/UserRepository.java
@@ -3,8 +3,20 @@ package com.example.springbackend.repository;
 import com.example.springbackend.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class UserRepository {
+    static List<User> userList = new ArrayList();
+
+
+    public static User saveUser(User user) {
+        user.setUserId(userList.size() + 1);
+        userList.add(user);
+        return user;
+    }
+
     public User getUser() {
         User user = new User("Rickey", "123 Sesame Street", 33);
         return user;
