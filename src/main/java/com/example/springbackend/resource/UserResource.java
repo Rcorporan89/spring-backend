35b9a01cca486b7394f51308b74b17ca8a900584
Rickey Corporan <Rcorporan89@gmail.com>
2023-02-09 17:00:02 -0500

    integrating the save user post endpoint with firebase auth

--- src/main/java/com/example/springbackend/resource/UserResource.java
+++ src/main/java/com/example/springbackend/resource/UserResource.java
@@ -1,14 +1,18 @@
 package com.example.springbackend.resource;
 
 import com.example.springbackend.exception.RestrictedInfoException;
+import com.example.springbackend.model.FirebaseUser;
 import com.example.springbackend.model.User;
+import com.example.springbackend.service.FirebaseService;
 import com.example.springbackend.service.UserService;
+import com.google.firebase.auth.FirebaseAuthException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -18,43 +22,39 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private FirebaseService firebaseService;
+
     @PostMapping
-    public User saveUser(@RequestBody @Valid User user) {
-        return userService.saveUser(user);
+    public User saveUser(@RequestBody @Valid User user, @RequestHeader(name = "idToken") String idToken) throws IOException, FirebaseAuthException {
+        FirebaseUser fireBaseUser = firebaseService.authenticate(idToken);
+        if (fireBaseUser != null) {
+            return userService.saveUser(user);
+        }
+        return null;
     }
 
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
-
+    @GetMapping("/find")
+    public List<User> getByAddress(@RequestParam(name = "address") String address) throws RestrictedInfoException {
+        if (address.equalsIgnoreCase("area51")) {
+            throw new RestrictedInfoException();
+        }
+        return userService.getByAddress(address);
+    }
+    @GetMapping("/find-by-id")
+    public User getById(@RequestParam("userId") String userId) {
+        return userService.getUserById(userId);
+    }
     @PutMapping
     public User updateUserById(@RequestBody User user) {
         return userService.updateUserById(user);
     }
-
     @DeleteMapping
-    public void deleteUser(@RequestParam(name="userId") String userId ) {
-         userService.deleteUser(userId);
+    public void deleteUser(@RequestParam(name = "userId") String userId) {
+        userService.deleteUser(userId);
     }
-
-    @GetMapping("/find")
-    public List <User> getByAddress(@RequestParam(name="address") String address) throws RestrictedInfoException {
-        if (address.equalsIgnoreCase("area51")){
-            throw new RestrictedInfoException();
-        }
-        return  userService.getByAddress(address);
-    }
-
-
-
-
-    @GetMapping("/find-by-id")
-    public User getUserById(@RequestParam("userId") String userId) {
-        return userService.getUserById(userId);
-    }
-
-
-
-
 }
