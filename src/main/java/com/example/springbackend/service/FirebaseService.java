375fa3ca803dffd3af37924df643a9cfbcbc8ea8
Rickey Corporan <Rcorporan89@gmail.com>
2023-02-14 17:16:35 -0500

    using a valid id token, created config package with configuration class alongside a bean method, fixes Firebase name [DEFAULT] already exists error.

--- src/main/java/com/example/springbackend/service/FirebaseService.java
+++ src/main/java/com/example/springbackend/service/FirebaseService.java
@@ -1,28 +1,13 @@
 package com.example.springbackend.service;
 
 import com.example.springbackend.model.FirebaseUser;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
-import java.io.FileInputStream;
 import java.io.IOException;
 @Service
 public class FirebaseService {
-    public FirebaseApp initializeFirebase() throws IOException {
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        Resource resource = resolver.getResource("classpath:backend-winter-23.json");
-        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://backend-winter-23.firebaseio.com")
-                .build();
-        return FirebaseApp.initializeApp(options);
-    }
+
 
     public FirebaseUser authenticate(String idToken) throws IOException, FirebaseAuthException {
 
